7051347c915f6e25a8bb8e3cb08c8b38ae934e56
9p4 <vcs@ersei.net>
2023-03-06 18:35:25 -0500

    Update to latest selenium

--- monkeytype.py
+++ monkeytype.py
@@ -2,10 +2,14 @@
 # -*- coding: utf-8 -*-
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 
 import signal
 import sys
+import time
+
+
 def signal_handler(signal, frame):
     print("exiting")
     driver.close()
@@ -15,7 +19,7 @@ def signal_handler(signal, frame):
 options = Options()
 options.binary = "/usr/bin/firefox"
 
-driver = webdriver.Firefox(options=options,executable_path="/usr/bin/geckodriver")
+driver = webdriver.Firefox(options=options, executable_path="/usr/bin/geckodriver")
 
 signal.signal(signal.SIGINT, signal_handler)
 
@@ -24,14 +28,17 @@ assert "Monkeytype" in driver.title
 while True:
     input("Press enter to start...")
     totype = ""
-    words = driver.find_elements_by_class_name("word")
+    words = driver.find_elements(By.CLASS_NAME, "word")
     for word in words:
-        for letter in word.find_elements_by_tag_name("letter"):
+        for letter in word.find_elements(By.TAG_NAME, "letter"):
             totype += letter.get_attribute("innerText")
         totype += " "
 
     # Begin typing
-    typebox = driver.find_element_by_id("wordsInput")
-    typebox.send_keys(totype)
+    typebox = driver.find_element(By.ID, "wordsInput")
+    for character in totype:
+        typebox.send_keys(character)
+        time.sleep(0.02)
+    # typebox.send_keys(totype)
 
     input("Press enter to run another set, press ^C or whatever to quit")
